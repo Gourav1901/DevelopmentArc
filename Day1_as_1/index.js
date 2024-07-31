79908dd07d2d5caebf0fbdfca5a3e47c6f6e25c5
Gourav1901 <gouravchoudhary2211@gmail.com>
2024-07-31 12:41:04 +0530

    task two done

--- Day1_as_1/index.js
+++ Day1_as_1/index.js
@@ -10,3 +10,12 @@ const fruits =  ["apple", "banana","orange"];
 const vegetables = ["carrot","broccoli","spinach"];
 console.log(combiningArrays(fruits, vegetables));
 
+
+function cloningObjects(person) {
+  personcopy = {...person};
+  return personcopy;
+}
+
+
+const person = { name: "John", age: 30, address: "123 Main St" };
+console.log(cloningObjects(person)); 
