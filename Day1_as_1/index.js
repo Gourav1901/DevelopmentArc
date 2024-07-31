e9b5f7fbb11a16c8cffd97d807f390a87bcffd1d
Gourav1901 <gouravchoudhary2211@gmail.com>
2024-07-31 12:50:53 +0530

    task done

--- Day1_as_1/index.js
+++ Day1_as_1/index.js
@@ -31,3 +31,24 @@ function mergingObjects(student,course) {
 const student = { name: "Alice", age: 20 };
 const course = { courseName: "Math", teacher: "Mr. Smith" };
 console.log(mergingObjects(student, course));
+
+
+function combiningNestedArrays(array1, array2) {
+  
+  const combinedArray = [...array1, ...array2];
+ 
+  return combinedArray;
+}
+
+// Example usage
+const array1 = [
+[1, 2],
+[3, 4],
+[5, 6],
+];
+const array2 = [
+[7, 8],
+[9, 10],
+[11, 12],
+];
+console.log(combiningNestedArrays(array1, array2)); 
