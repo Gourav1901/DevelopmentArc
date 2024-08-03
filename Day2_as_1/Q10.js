ec13e5a069f4408e057b946b8cc042323baf543a
Gourav1901 <gouravchoudhary2211@gmail.com>
2024-08-03 19:53:29 +0530

    complete all the problems

--- Day2_as_1/Q10.js
+++ Day2_as_1/Q10.js
@@ -278,3 +278,56 @@ const voters = [
 
 console.log(countingVoter(voters));
 
+function countFavoriteIceCreams(data) {
+  return data.reduce((acc, person) => {
+    person.favoriteIceCreams.forEach((flavor) => {
+      acc[flavor] = (acc[flavor] || 0) + 1;
+    });
+    return acc;
+  }, {});
+}
+
+const data = [
+  {
+    name: "Superman",
+    favoriteIceCreams: [
+      "Strawberry",
+      "Vanilla",
+      "Chocolate",
+      "Cookies & Cream",
+    ],
+  },
+  {
+    name: "Batman",
+    favoriteIceCreams: [
+      "Cookies & Cream",
+      "Mint Chocolate Chip",
+      "Chocolate",
+      "Vanilla",
+    ],
+  },
+  {
+    name: "Flash",
+    favoriteIceCreams: ["Chocolate", "Rocky Road", "Pistachio", "Banana"],
+  },
+  {
+    name: "Aquaman",
+    favoriteIceCreams: ["Vanilla", "Chocolate", "Mint Chocolate Chip"],
+  },
+  {
+    name: "Green Lantern",
+    favoriteIceCreams: [
+      "Vanilla",
+      "French Vanilla",
+      "Vanilla Bean",
+      "Strawberry",
+    ],
+  },
+  {
+    name: "Robin",
+    favoriteIceCreams: ["Strawberry", "Chocolate", "Mint Chocolate Chip"],
+  },
+];
+
+const res = countFavoriteIceCreams(data);
+console.log(res);
