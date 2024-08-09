ffb23993a6f00f5fe753c57655939a07fedfdbe7
Gourav1901 <gouravchoudhary2211@gmail.com>
2024-08-09 09:10:44 +0530

    update the code

--- w2Day3/q4/Q4_1.index.js
+++ w2Day3/q4/Q4_1.index.js
@@ -1,18 +1,38 @@
 function codingScoreCheck(marks, cutoff) {
   return new Promise((resolve, reject) => {
       setTimeout(() => {
-          // Calculate the average score
+          
           const total = marks.reduce((sum, mark) => sum + mark, 0);
           const average = total / marks.length;
           
-          // Check if the average score meets or exceeds the cutoff
+          
           if (average >= cutoff) {
               resolve(average);
           } else {
               reject("Sorry you haven’t cleared the coding round.");
           }
-      }, 2000); // 2 seconds delay
+      }, 2000); 
   });
+}
+
+
+
+function HUKUMUScoreCheck(marks, cutoff){
+  return new Promise((resolve,reject) =>{
+    setTimeout(()=>{
+      const total  = marks.reduce((sum,cutoff)=> sum+cutoff,0);
+      const average = total/marks.length
+
+
+    if(average >= cutoff){
+      resolve("congratulation")
+    }
+    else{
+      reject("Sorry you haven’t cleared the coding round.");
+    }
+
+    },2000)
+  })
 }
 
 // Example usage
@@ -22,3 +42,8 @@ const cutoff = 75;
 codingScoreCheck(marks, cutoff)
   .then(average => console.log(`Congratulations! Your average score is ${average}.`))
   .catch(error => console.log(error));
+
+
+HUKUMUScoreCheck(marks,cutoff)
+  .then(average => console.log(`Congratulations! You have clear the Interview`))
+  .catch(error => console.log(`error`));
